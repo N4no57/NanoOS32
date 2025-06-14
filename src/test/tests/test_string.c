27294eaa3698ddd2a2009dec08ad637a6d0538fb
NanoDev <b.rego.oliveira@gmail.com>
2025-06-14 22:48:03 +0100

    created unit test for string.h

---
+++ src/test/tests/test_string.c
@@ -0,0 +1,9 @@
+#include <assert.h>
+#include <string.h>
+#include <test_string.h>
+
+void test_strlen() {
+    ASSERT(strlen("") == 0);
+    ASSERT(strlen("abc") == 3);
+    test_pass("test_strlen");
+}
