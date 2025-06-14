23ae4ce76b6621d49d70426a13eb92f5a76479d2
NanoDev <b.rego.oliveira@gmail.com>
2025-06-14 17:10:40 +0100

    defined strcmp(), strcpy(), memset() and memcpy() in string.h

--- src/intf/string.h
+++ src/intf/string.h
@@ -5,5 +5,10 @@
 #include <stddef.h>
 
 size_t strlen(const char* str);
+bool strcmp(const char* a, const char* b);
+void strcpy(char* dest, const char* src);
+
+void memset(void* dest, int c, size_t n);
+void memcpy(void* dest, const void* src, size_t count);
 
 #endif //STRING_H
