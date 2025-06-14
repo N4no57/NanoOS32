7ca4e749f7aa42492b4b06a3f34c9295d34969bf
NanoDev <b.rego.oliveira@gmail.com>
2025-06-14 21:40:45 +0100

    removed define and added include to terminal.h in main.c

--- src/impl/kernel/main.c
+++ src/impl/kernel/main.c
@@ -3,21 +3,18 @@
 #include <stdint.h>
 #include <string.h>
 #include <stdio.h>
+#include <terminal.h>
 
 /* Check if the compiler thinks you are targeting the wrong operating system. */
 #if defined(__linux__)
 #error "You are not using a cross-compiler, you will most certainly run into trouble"
 #endif
 
-/* This tutorial will only work for the 32-bit ix86 targets. */
-#if not defined(__i386__)
-#error "This tutorial needs to be compiled with a ix86-elf compiler"
-#endif
 
 void kernel_main(void) {
 	/* Initialize terminal interface */
 	terminal_initialize();
 
 	printf("Hello, kernel World!\n");
-	printf("Are you ready to rumble!!\n");
+    printf("Are you ready to rumble!!\n");
 }
