88dc09425de5caf910197b3a1b9fc48b0d9c8344
NanoDev <b.rego.oliveira@gmail.com>
2025-06-14 16:48:30 +0100

    added newlines to the OS

--- src/impl/kernel/main.c
+++ src/impl/kernel/main.c
@@ -59,8 +59,7 @@ size_t terminal_column;
 uint8_t terminal_color;
 uint16_t* terminal_buffer = (uint16_t*)VGA_MEMORY;
 
-void terminal_initialize(void) 
-{
+void terminal_initialize(void) {
 	terminal_row = 0;
 	terminal_column = 0;
 	terminal_color = vga_entry_color(VGA_COLOR_LIGHT_GREY, VGA_COLOR_BLACK);
@@ -73,43 +72,46 @@ void terminal_initialize(void)
 	}
 }
 
-void terminal_setcolor(uint8_t color) 
-{
+void terminal_setcolor(uint8_t color) {
 	terminal_color = color;
 }
 
-void terminal_putentryat(char c, uint8_t color, size_t x, size_t y) 
-{
+void terminal_putentryat(char c, uint8_t color, size_t x, size_t y) {
 	const size_t index = y * VGA_WIDTH + x;
 	terminal_buffer[index] = vga_entry(c, color);
 }
 
-void terminal_putchar(char c) 
-{
+void terminal_putchar(char c) {
 	terminal_putentryat(c, terminal_color, terminal_column, terminal_row);
 	if (++terminal_column == VGA_WIDTH) {
 		terminal_column = 0;
-		if (++terminal_row == VGA_HEIGHT)
+		if (++terminal_row == VGA_HEIGHT) {
 			terminal_row = 0;
+		}
 	}
 }
 
-void terminal_write(const char* data, size_t size) 
-{
-	for (size_t i = 0; i < size; i++)
-		terminal_putchar(data[i]);
+void terminal_write(const char* data, size_t size) {
+	for (size_t i = 0; i < size; i++) {
+        if (data[i] == '\n') {
+            terminal_row++;
+            terminal_column = 0;
+        	continue;
+    	} else {
+        	terminal_putchar(data[i]);
+        }
+    }
 }
 
-void terminal_writestring(const char* data) 
-{
+void terminal_writestring(const char* data) {
 	terminal_write(data, strlen(data));
 }
 
-void kernel_main(void) 
-{
+void kernel_main(void) {
 	/* Initialize terminal interface */
 	terminal_initialize();
 
 	/* Newline support is left as an exercise. */
 	terminal_writestring("Hello, kernel World!\n");
+    terminal_writestring("Are you ready to rumble!!\n");
 }
