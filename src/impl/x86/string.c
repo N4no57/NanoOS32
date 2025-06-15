b4fc01b15ee9123870b9e7d3d40b2234e5b8645b
NanoDev <b.rego.oliveira@gmail.com>
2025-06-15 20:03:47 +0100

    implemented memset() in string.h

--- src/impl/x86/string.c
+++ src/impl/x86/string.c
@@ -24,3 +24,12 @@ char *strcpy(char* dest, const char* src) {
     } while (src[i++] != '\0');
     return dest;
 }
+
+void *memset(void* dest, int c, size_t n) {
+    unsigned char *ptr = (unsigned char) dest;
+    unsigned char byte_val = (unsigned char) c;
+    for (size_t i = 0; i < n; i++) {
+        ptr[i] = byte_val;
+    }
+    return dest;
+}
--- src/test/testlib/assert.c
+++ src/test/testlib/assert.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 void test_fail(const char* file, int line, const char* expr) {
-    printf("[FAIL] %s at %s : %d", expr, file, line);
+    printf("[FAIL] %s at '%s' : %d", expr, file, line);
     for (;;);
 }
 
