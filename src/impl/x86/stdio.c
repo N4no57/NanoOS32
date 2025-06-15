6778ca1fe633c5494172e9f3ab83829d30cbfff3
NanoDev <b.rego.oliveira@gmail.com>
2025-06-15 19:54:03 +0100

    skipped writing '\0' in specifiers switch

--- src/impl/x86/stdio.c
+++ src/impl/x86/stdio.c
@@ -41,8 +41,7 @@ int printf(const char* _Format, ...) {
 
     do {
         if(_Format[i] != '%') {
-            out_str[out_idx] = _Format[i];
-            out_idx++;
+            out_str[out_idx++] = _Format[i];
         } else {
             i++;
             char specifier = _Format[i];
@@ -56,19 +55,22 @@ int printf(const char* _Format, ...) {
                     int num_idx = 0;
                     itoa(val, num_buf);
                     do {
+                        if (num_buf[num_idx] == '\0')
+                            break;
                         out_str[out_idx++] = num_buf[num_idx];
                     } while (num_buf[num_idx++] != '\0');
                     break;
                 case 'c':
                     val = va_arg(args, int);
-                    out_str[out_idx] = (char)val;
-                    out_idx++;
+                    out_str[out_idx++] = (char)val;
                     break;
                 case 's':
                     string = va_arg(args, char*);
                     int string_idx = 0;
                     do {
-                        out_str[out_idx] = string[string_idx];
+                        if (string[string_idx] == '\0')
+                            break;
+                        out_str[out_idx++] = string[string_idx];
                     } while (string[string_idx++] != '\0');
                     break;
             }
