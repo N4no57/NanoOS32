9b5bb82e6d2287d5db6152be8f863ab1e937a3b3
NanoDev <b.rego.oliveira@gmail.com>
2025-06-21 23:34:35 +0100

    created external symbols for the linker. created the heap and heap_init(). created malloc() and free().

--- src/impl/x86/stdlib/heap.c
+++ src/impl/x86/stdlib/heap.c
@@ -1,6 +1,9 @@
 #include <stdint.h>
 #include <stdbool.h>
 
+extern uint8_t heap_start;
+extern uint8_t heap_end;
+
 struct heapinfo_t {
     struct heapchunk_t *start;
     uint32_t avail;
@@ -11,3 +14,22 @@ struct heapchunk_t {
     bool inuse;
     struct heapchunk_t *next;
 };
+
+static struct heapinfo_t heap;
+
+void heap_init(void) {
+    heap.start = (struct heapchunk_t*)(uintptr_t)&heap_start;
+    heap.avail = (uintptr_t)&heap_end - (uintptr_t)&heap_start;
+
+    heap.start->size = heap.avail - sizeof(struct heapchunk_t);
+    heap.start->inuse = false;
+    heap.start->next = NULL;
+}
+
+void* malloc(size_t size) {
+    
+}
+
+void* free(void* ptr) {
+
+}
