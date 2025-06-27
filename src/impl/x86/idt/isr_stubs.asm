81a1e07f242b56aea57d29e7dcfed7c99666bae6
NanoDev <110634754+N4no57@users.noreply.github.com>
2025-06-27 02:02:30 +0100

    moved irq components to separate file

--- src/impl/x86/idt/isr_stubs.asm
+++ src/impl/x86/idt/isr_stubs.asm
@@ -1,6 +1,5 @@
 [BITS 32]
 extern exception_handler
-extern irq_handler
 
 ; ISR without error code
 %macro isr_no_err_stub 1
@@ -18,17 +17,6 @@ isr_stub_%1:
     add esp, 4
     iret
 %endmacro
-
-; IRQ no error code
-%macro irq_no_err_stub 1
-global irq_stub_%1
-irq_stub_%1
-    push %1+32
-    call irq_handler
-    add esp, 4
-    iret
-%endmacro
-
 
 ; Exceptions 0–31
 isr_no_err_stub 0
@@ -64,38 +52,11 @@ isr_no_err_stub 29
 isr_err_stub    30
 isr_no_err_stub 31
 
-; IRQs 0–15 (vectors 32–47)
-irq_no_err_stub 0
-irq_no_err_stub 1
-irq_no_err_stub 2
-irq_no_err_stub 3
-irq_no_err_stub 4
-irq_no_err_stub 5
-irq_no_err_stub 6
-irq_no_err_stub 7
-irq_no_err_stub 8
-irq_no_err_stub 9
-irq_no_err_stub 10
-irq_no_err_stub 11
-irq_no_err_stub 12
-irq_no_err_stub 13
-irq_no_err_stub 14
-irq_no_err_stub 15
-
 global isr_stub_table
-global irq_stub_table
 section .data
-
 isr_stub_table:
 %assign i 0 
 %rep    32 
     dd isr_stub_%+i
 %assign i i+1 
-%endrep
-
-irq_stub_table:
-%assign i 0
-%rep 16
-    dd irq_stub_%+i
-%assign i i+1
 %endrep
