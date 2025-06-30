78cc3338a649745ed038ad17a3c6d51741defec6
NanoDev <110634754+N4no57@users.noreply.github.com>
2025-06-30 16:17:51 +0100

    changed frequency of PIT to 100 Hz

--- src/impl/kernel/main.c
+++ src/impl/kernel/main.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <idt.h>
 #include <pic.h>
+#include <pit.h>
 
 /* Check if the compiler thinks you are targeting the wrong operating system. */
 #if defined(__linux__)
@@ -11,9 +12,10 @@
 
 
 void kernel_main(void) {
-	/* Initialize kernel code */
+	/* Initialise kernel code */
 	terminal_initialize();
 	heap_init();
+	set_frequency(11932);
 	idt_init();
 	PIC_remap(PIC1, PIC2);
 
