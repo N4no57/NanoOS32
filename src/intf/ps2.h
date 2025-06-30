861223b03b25fdadd97df7f019e7127c6aa9b723
NanoDev <b.rego.oliveira@gmail.com>
2025-06-30 21:11:48 +0100

    added an input buffer and corresponding read and write pointers

--- src/intf/ps2.h
+++ src/intf/ps2.h
@@ -1,6 +1,10 @@
 #ifndef PS2_H
 #define PS2_H
 
+extern volatile unsigned char input_buff[256];
+extern volatile unsigned char read_ptr;
+extern volatile unsigned char write_ptr;
+
 void keyboard_interrupt_handler();
 
 #endif
