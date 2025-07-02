93c42059af6122bee4643f2497e721a3abf4f150
NanoDev <b.rego.oliveira@gmail.com>
2025-07-02 20:43:16 +0100

    created KeyEvent struct, defined get_key_event() function

--- src/intf/stdio.h
+++ src/intf/stdio.h
@@ -2,6 +2,15 @@
 #define STDIO_H
 
 #include <stddef.h>
+#include <stdint.h>
+
+typedef struct {
+    enum { KEY_ASCII, KEY_SPECIAL } type;
+    union {
+        char ascii;
+        uint16_t code; // e.g., KEY_LEFT, KEY_PGUP
+    };
+} KeyEvent;
 
 // Legacy functions
 int legacy_printf(const char* _Format, ...);
@@ -10,6 +19,7 @@ char *fgets(char *buffer, size_t max_len);
 
 // maintained functions
 int printf(const char* _Format, ...);
+KeyEvent get_key_event();
 
 static inline unsigned char inb(unsigned short port) {
     unsigned char ret;
