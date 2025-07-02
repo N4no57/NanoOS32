be46f951cca2414943ac8b5a2b301bea683c681b
NanoDev <b.rego.oliveira@gmail.com>
2025-07-02 20:56:48 +0100

    implemented basic event checks

--- src/impl/x86/stdio/readline.c
+++ src/impl/x86/stdio/readline.c
@@ -1,5 +1,19 @@
 #include "../../../intf/stdio.h"
 
 void readline(char *buffer, size_t max_len) {
-    
+    size_t len = 0;
+
+    while (1) {
+        KeyEvent ev = get_key_event();
+        
+        if (ev.type == ERROR) {
+            return;
+        }
+
+        if (ev.type == KEY_ASCII) {
+
+        } else if (ev.type == KEY_SPECIAL) {
+            
+        }
+    }
 }
