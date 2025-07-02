5f64ad52ea5b04eb56bf8383967c9af2440b3675
NanoDev <b.rego.oliveira@gmail.com>
2025-07-02 20:26:24 +0100

    exposed the parsed buffer and it subsequent pointers

--- src/intf/ps2.h
+++ src/intf/ps2.h
@@ -1,9 +1,9 @@
 #ifndef PS2_H
 #define PS2_H
 
-extern volatile unsigned char input_buff[256];
-extern volatile unsigned char read_ptr;
-extern volatile unsigned char write_ptr;
+extern volatile unsigned char parsed_buff[256];
+extern volatile unsigned char parsed_read_ptr;
+extern volatile unsigned char parsed_write_ptr;
 
 void keyboard_interrupt_handler();
 void parse_input_buffer();
