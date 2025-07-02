b66410b7221c6bb58ed511cc58cd152974826f43
NanoDev <b.rego.oliveira@gmail.com>
2025-07-02 20:30:43 +0100

    exposed key event buffer and subsequent pointers

--- src/intf/ps2.h
+++ src/intf/ps2.h
@@ -5,6 +5,10 @@ extern volatile unsigned char parsed_buff[256];
 extern volatile unsigned char parsed_read_ptr;
 extern volatile unsigned char parsed_write_ptr;
 
+extern volatile unsigned char key_event[256];
+extern volatile unsigned char key_event_read_ptr;
+extern volatile unsigned char key_event_write_ptr;
+
 void keyboard_interrupt_handler();
 void parse_input_buffer();
 
