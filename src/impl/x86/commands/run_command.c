fae37e7a9649a4ebcc7ba7ad0dd5c56c4ad0a821
NanoDev <b.rego.oliveira@gmail.com>
2025-07-05 13:42:56 +0100

    moved definition of commands[] and command count

--- src/impl/x86/commands/run_command.c
+++ src/impl/x86/commands/run_command.c
@@ -1,6 +1,12 @@
 #include "../../../intf/commands.h"
 #include "../../../intf/string.h"
 
+command_entry_t commands[] = {
+    { "help", cmd_help },
+};
+
+int command_count = sizeof(commands) / sizeof(commands[0]);
+
 int run_command(int argc, char **argv) {
     char name[strlen(argv[0])];
     strcpy(name, argv[0]);
--- src/intf/commands.h
+++ src/intf/commands.h
@@ -1,16 +1,12 @@
 typedef int (*command_func_t)(int argc, char **argv);
 
-int cmd_help(int argc, char **argv);
-
 typedef struct {
     const char* name;
     command_func_t func;
 } command_entry_t;
 
-command_entry_t commands[] = {
-    { "help", cmd_help }
-};
-
-const int command_count = sizeof(commands) / sizeof(commands[0]);
+extern command_entry_t commands[];
+extern int command_count;
 
 int run_command(int argc, char **argv);
+int cmd_help(int argc, char **argv);
