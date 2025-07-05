4c0d79fc49d8ad047d3917739a51e36b154ed3de
NanoDev <b.rego.oliveira@gmail.com>
2025-07-05 12:42:35 +0100

    added help entry

--- src/intf/commands.h
+++ src/intf/commands.h
@@ -5,8 +5,8 @@ typedef struct {
     command_func_t func;
 } command_entry_t;
 
-command_entry_t commands[] {
-
+command_entry_t commands[] = {
+    { "help", cmd_help }
 };
 
 const int command_count = sizeof(commands) / sizeof(commands[0]);
