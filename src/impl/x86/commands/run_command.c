116480ce794d732c813a9a30e5c2e8af5bcb07eb
NanoDev <b.rego.oliveira@gmail.com>
2025-07-05 12:50:03 +0100

    fixed invalid syntax and import error

--- src/impl/x86/commands/run_command.c
+++ src/impl/x86/commands/run_command.c
@@ -1,8 +1,9 @@
 #include "../../../intf/commands.h"
-#include "../../../intf/stdio.h"
+#include "../../../intf/string.h"
 
 int run_command(int argc, char **argv) {
-    char name[] = argv[0];
+    char name[strlen(argv[0])];
+    strcpy(name, argv[0]);
     for (int i = 0; i < command_count; i++) {
         if (strcmp(name, commands[i].name) == 0) {
             return commands[i].func(argc, argv);
