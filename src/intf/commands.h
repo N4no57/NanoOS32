3a023d293678c97372e411f5febf5d7307c9a68c
NanoDev <b.rego.oliveira@gmail.com>
2025-07-05 12:41:01 +0100

    commands.h for command modularization

---
+++ src/intf/commands.h
@@ -0,0 +1,12 @@
+typedef int (*command_func_t)(int argc, char **argv);
+
+typedef struct {
+    const char* name;
+    command_func_t func;
+} command_entry_t;
+
+command_entry_t commands[] {
+
+};
+
+const int command_count = sizeof(commands) / sizeof(commands[0]);
