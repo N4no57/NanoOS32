62e651fe985315184aaf63ca2ab0192bc45116ac
NanoDev <b.rego.oliveira@gmail.com>
2025-07-05 16:19:20 +0100

    fixed minor bug

--- src/impl/x86/commands/run_command.c
+++ src/impl/x86/commands/run_command.c
@@ -15,7 +15,7 @@ command_entry_t commands[] = {
 int command_count = sizeof(commands) / sizeof(commands[0]);
 
 int run_command(int argc, char **argv) {
-    char name[strlen(argv[0])+1];
+    char name[strlen(argv[0])];
     strcpy(name, argv[0]);
     for (int i = 0; i < command_count; i++) {
         if (strcmp(name, commands[i].name) == 0) {
