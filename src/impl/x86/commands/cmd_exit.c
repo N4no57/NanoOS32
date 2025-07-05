95b8a5cd8771e2f870fff540a1d67d017a57e7fa
NanoDev <b.rego.oliveira@gmail.com>
2025-07-05 14:15:27 +0100

    added a simple halting for cmd_exit()

--- src/impl/x86/commands/cmd_exit.c
+++ src/impl/x86/commands/cmd_exit.c
@@ -1,5 +1,11 @@
 #include "../../../intf/commands.h"
+#include "../../../intf/stdio.h"
+
+#define SLP_TYP   0x2000
+#define SLP_EN    0x2000
 
 int cmd_exit(int argc, char **argv) {
-    
+    printf("Shutting down...");
+
+    for (;;) asm volatile ("hlt");
 }
