52a183a56402afeae62b9daf3708277c6670251c
NanoDev <b.rego.oliveira@gmail.com>
2025-07-05 14:51:33 +0100

    implemented cmd_clear

--- src/impl/x86/commands/cmd_clear.c
+++ src/impl/x86/commands/cmd_clear.c
@@ -1,5 +1,13 @@
 #include "../../../intf/commands.h"
+#include "../../../intf/terminal.h"
+#include "../../../intf/stdio.h"
 
 int cmd_clear(int argc, char **argv) {
-    
+    if (argc >= 2) {
+        printf("Usage: clear\n\n");
+        return -1;
+    }
+
+    terminal_clear();
+    return 0;
 }
