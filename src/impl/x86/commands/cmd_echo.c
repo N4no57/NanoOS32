03f8b5208594934abb7b76ed4b87835b4d202978
NanoDev <b.rego.oliveira@gmail.com>
2025-07-05 15:43:42 +0100

    implemented cmd_echo() fully

--- src/impl/x86/commands/cmd_echo.c
+++ src/impl/x86/commands/cmd_echo.c
@@ -5,7 +5,7 @@
 int cmd_echo(int argc, char **argv) {
     int total_len = 0;
     
-    for (int i = 0; argv[i] != NULL; i++) {
+    for (int i = 1; argv[i] != NULL; i++) {
         total_len += strlen(argv[i]);
     }
     total_len += (argc - 1) - 1; // for space between the words
@@ -13,6 +13,13 @@ int cmd_echo(int argc, char **argv) {
 
     char out_str[total_len];
     out_str[0] = '\0';
+    for (int i = 1; argv[i] != NULL; i++) {
+        strcat(out_str, argv[i]);
+        if (argv[i + 1] != NULL) {
+            strcat(out_str, " ");
+        }
+    }
 
     printf("%s\n", out_str);
+    return 0;
 }
