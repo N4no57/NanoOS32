f6f347c0a82ad6e8ebee7d082d7f3d7f94e01f0a
NanoDev <b.rego.oliveira@gmail.com>
2025-07-05 15:53:20 +0100

    implemented placeholder reboot implementation

---
+++ src/impl/x86/commands/cmd_reboot.c
@@ -0,0 +1,8 @@
+//! THIS ISN'T ACTUAL IMPLEMENTATION ONLY A PLACEHOLDER
+//! REPEAT THIS IS ONLY A PLACEHOLDER
+#include "../../../intf/commands.h"
+#include "../../../intf/stdio.h"
+
+int cmd_reboot(int argc, char **argv) {
+    printf("not implemented yet");
+}
