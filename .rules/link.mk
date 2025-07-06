67c49d70f2cfc0b2c6cc4abb0c09b43ab003965a
NanoDev <b.rego.oliveira@gmail.com>
2025-07-06 20:16:31 +0100

    changed directory to match new directory of linker.ld

--- .rules/link.mk
+++ .rules/link.mk
@@ -1,2 +1,2 @@
-build-x86: headers
-	$(CROSS_GCC) -o build/x86/kernel.elf -T targets/x86/linker.ld $(LDFLAGS) -ffreestanding -nostdlib $(kernel_object_files) $(x86_object_files)
+build-x86: build-objs
+	$(CROSS_GCC) -o build/x86/kernel.elf -T linker.ld $(LDFLAGS) -ffreestanding -nostdlib $(kernel_object_files) $(x86_object_files)
