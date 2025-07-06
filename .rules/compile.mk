0af0337b42cc6c8a1c5581e4ff3ca7b16ec47140
NanoDev <b.rego.oliveira@gmail.com>
2025-07-06 20:23:14 +0100

    removed ShitOS directory in build directory

--- .rules/compile.mk
+++ .rules/compile.mk
@@ -1,19 +1,19 @@
-$(kernel_object_files): build/ShitOS/kernel/%.o : src/impl/kernel/%.c
+$(kernel_object_files): build/kernel/%.o : src/impl/kernel/%.c
 	mkdir -vp $(dir $@)
 	$(CROSS_GCC) -c $(CFLAGS) $< -o $@
 
-$(driver_c_object_files): build/ShitOS/drivers/%.o : src/impl/drivers/%.c
+$(driver_c_object_files): build/drivers/%.o : src/impl/drivers/%.c
 	mkdir -vp $(dir $@)
 	$(CROSS_GCC) -c $(CFLAGS) $< -o $@
 
-$(x86_c_object_files): build/ShitOS/x86/%.o : src/impl/x86/%.c
+$(x86_c_object_files): build/x86/%.o : src/impl/x86/%.c
 	mkdir -vp $(dir $@)
 	$(CROSS_GCC) -c $(CFLAGS) $< -o $@
 
-$(x86_asm_object_files): build/ShitOS/x86/%.o : src/impl/x86/%.s
+$(x86_asm_object_files): build/x86/%.o : src/impl/x86/%.s
 	mkdir -vp $(dir $@)
 	$(CROSS_AS) $< -o $@
 
-$(x86_nasm_object_files): build/ShitOS/x86/%.o : src/impl/x86/%.asm
+$(x86_nasm_object_files): build/x86/%.o : src/impl/x86/%.asm
 	mkdir -vp $(dir $@)
 	nasm -f elf32 $< -o $@
