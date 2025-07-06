e529dc05260838380cc05b1f2e6bdb6cde9774d8
NanoDev <b.rego.oliveira@gmail.com>
2025-07-06 20:22:50 +0100

    moved kernel.elf out og x86 dir

--- .rules/link.mk
+++ .rules/link.mk
@@ -1,2 +1,2 @@
 build-x86: build-objs
-	$(CROSS_GCC) -o build/x86/kernel.elf -T linker.ld $(LDFLAGS) -ffreestanding -nostdlib $(kernel_object_files) $(x86_object_files)
+	$(CROSS_GCC) -o build/kernel.elf -T linker.ld $(LDFLAGS) -ffreestanding -nostdlib $(kernel_object_files) $(x86_object_files)
