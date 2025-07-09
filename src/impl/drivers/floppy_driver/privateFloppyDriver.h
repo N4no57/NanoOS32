0447192d546391df233d0510b64d6f7b0363b681
NanoDev <b.rego.oliveira@gmail.com>
2025-07-09 21:43:12 +0100

    created private header for the floppy disk driver

---
+++ src/impl/drivers/floppy_driver/privateFloppyDriver.h
@@ -0,0 +1,19 @@
+#include "../../../intf/stdio.h"
+
+enum FloppyRegisters
+{
+   STATUS_REGISTER_A                = 0x3F0, // read-only
+   STATUS_REGISTER_B                = 0x3F1, // read-only
+   DIGITAL_OUTPUT_REGISTER          = 0x3F2,
+   TAPE_DRIVE_REGISTER              = 0x3F3,
+   MAIN_STATUS_REGISTER             = 0x3F4, // read-only
+   DATARATE_SELECT_REGISTER         = 0x3F4, // write-only
+   DATA_FIFO                        = 0x3F5,
+   DIGITAL_INPUT_REGISTER           = 0x3F7, // read-only
+   CONFIGURATION_CONTROL_REGISTER   = 0x3F7  // write-only
+};
+
+static inline unsigned char read_cmos(unsigned char reg) {
+    outb(0x70, reg | 0x80);
+    return inb(0x71);
+}
