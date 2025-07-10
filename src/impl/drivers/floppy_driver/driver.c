1aa8cb00c58ea4270d1a449108be827f31ee047d
NanoDev <b.rego.oliveira@gmail.com>
2025-07-10 11:50:14 +0100

    work on FDC_init()

--- src/impl/drivers/floppy_driver/driver.c
+++ src/impl/drivers/floppy_driver/driver.c
@@ -7,3 +7,9 @@ void test() {
     printf("value: %d\n", read_cmos(0x12));
     printf("value: %d\n", read_cmos(0x13));
 }
+
+void FDC_init() {
+    outb(DIGITAL_OUTPUT_REGISTER, 0x00);
+    //sleep(10);
+    outb(DIGITAL_OUTPUT_REGISTER, 0b00011100);
+}
