d25d74a504cea4644f0c478ec4d3e7951c0625c8
NanoDev <b.rego.oliveira@gmail.com>
2025-07-14 15:03:19 +0100

    fixed get uptime function

--- src/impl/x86/time/get_uptime.c
+++ src/impl/x86/time/get_uptime.c
@@ -1,5 +1,5 @@
 #include "privateTime.h"
 
-uint32_t get_uptime() {
-    return tick;
+uint32_t get_uptime() { // ticks are counted every 100 Hz so 1/100 seconds, so dividing by 100 gives you a second
+    return tick / 100;
 }
